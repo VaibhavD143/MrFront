0a722a55316dc8d5ef244cd015a18c65f9ac89d9
VaibhavD143 <vaibhav.dodiya143vd@gmail.com>
2020-12-04 03:10:26 +0530

    url updated

--- src/app/utils/global-constants.ts
+++ src/app/utils/global-constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants {
   // public static baseUrl : String = "http://localhost:8010";
-  public static baseUrl : String = "http://52.172.150.44:8010/";
+  public static baseUrl : String = "http://52.172.150.44:8010";
 
   public static loginUrl = GlobalConstants.baseUrl+"/login";
   public static findAllUOMsUrl = GlobalConstants.baseUrl+"/admin/uom";
